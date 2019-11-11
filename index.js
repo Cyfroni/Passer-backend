07b334643ecf6227882a6479f8ba4c1837aecb80
Karol <karolplg@gmail.com>
2019-11-11 16:08:29 +0100

    reformat

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const multiaddr = require("multiaddr");
 const PeerInfo = require("peer-info");
-const fs = require("fs");
-const pull = require("pull-stream");
 const { Node } = require("./models/node");
-const crypto = require("crypto");
-const _ = require("underscore");
-const toPull = require("stream-to-pull-stream");
-const Readable = require("stream").Readable;
-const glob = require("glob");
-const Cid = require("cids");
 
 const rl = require("readline").createInterface({
   input: process.stdin,
@@ -31,42 +23,7 @@ function createPeer(callback) {
     // register an event handler for errors.
     // here we're just going to print and re-throw the error
     // to kill the program
-    peer.on("error", err => {
-      console.error("libp2p error: ", err);
-      throw err;
-    });
-
-    peer.on("peer:connect", peer2 => {
-      console.log("Connection established to:", peer2.id.toB58String());
-      const key = Buffer.from("hello-key");
-      const value = Buffer.from("hello-value");
-      peer.dht.put(key, value);
-      peer.dht.get(key, (err, buffer) => {
-        console.log("DHT is " + buffer);
-      });
-    });
-
-    peer.handle("/file/1.0.0", (protocolName, connection) => {
-      pull(
-        connection,
-        pull.collect((err, data) => {
-          console.log("received:", data);
-          var num = data[0];
-          var hash = data[1].toString();
-          var shard = data[2].toString();
-          var id = peer.peerInfo.id.toB58String();
-          fs.writeFile(`shards/${id}_${hash}_${num}`, shard, function(err) {
-            if (err) throw err;
-            console.log("File is created successfully.");
-            // peer.contentRouting.provide(new Cid(1, "raw", data[1]), err => {
-            //   if (err) throw err;
-
-            //   console.log("Node %s is providing %s", id, hash);
-            // });
-          });
-        })
-      );
-    });
+    peer.addListeners();
 
     callback(null, peer);
   });
@@ -85,8 +42,8 @@ function handleStart(peer) {
   rl.setPrompt("command> ");
   rl.prompt();
 
-  rl.on("line", function(line) {
-    processCommand(line, peer);
+  rl.on("line", async function(line) {
+    await peer.processCommand(line);
     rl.prompt();
   }).on("close", function() {
     process.exit(0);
@@ -107,90 +64,3 @@ createPeer((err, peer) => {
     handleStart(peer);
   });
 });
-
-function splitString(str, n) {
-  var chunks = [];
-  var quantity = Math.ceil(str.length / n);
-  for (var i = 0, charsLength = str.length; i < charsLength; i += quantity) {
-    chunks.push(str.substring(i, i + quantity));
-  }
-  return chunks;
-}
-
-function addMetaData(peer, hash) {
-  var key = Buffer.from("0");
-  console.log(key);
-  peer.dht.get(key, (err, buffer) => {
-    console.log(buffer);
-    var metaData = buffer ? JSON.parse(buffer) : [];
-    metaData.push(hash);
-    peer.dht.put(key, Buffer.from(JSON.stringify(metaData)));
-  });
-}
-
-function processCommand(command, peer) {
-  console.log("Command was: " + command);
-  args = command.split(" ");
-  console.log("Split: " + args[0] + args[1]);
-
-  if (args[0] == "dht") {
-    if (args[1] == "put") {
-      const key = Buffer.from(args[2]);
-      const value = Buffer.from(args[3]);
-      peer.dht.put(key, value);
-      console.log(`DHT PUT ${key}:${value}`);
-    }
-    if (args[1] == "get") {
-      const key = Buffer.from(args[2]);
-      peer.dht.get(key, (err, buffer) => {
-        console.log(`DHT GET ${key}:${buffer}`);
-      });
-    }
-  }
-  if (args[0] == "store") {
-    const data = fs.readFileSync(args[1], "utf8");
-    console.log(data);
-    const dataHash = crypto
-      .createHash("md5")
-      .update(data)
-      .digest("hex");
-    const chunkQuantity = 2;
-    const chunks = splitString(data, chunkQuantity);
-    var peers = peer.peerBook.getAllArray();
-    _.sample(peers, chunkQuantity).forEach((p, i) => {
-      var s = new Readable();
-      s.push(i.toString());
-      s.push(dataHash);
-      s.push(chunks[i]);
-      s.push(null);
-
-      peer.dialProtocol(p, "/file/1.0.0", (err, connection) => {
-        pull(toPull.duplex(s), connection);
-      });
-    });
-    addMetaData(peer, dataHash);
-  }
-  if (args[0] == "getPeerChunks") {
-    var id = peer.peerInfo.id.toB58String();
-
-    glob(`shards/${id}*`, function(er, files) {
-      files.forEach(file => {
-        console.log(file, "\n", fs.readFileSync(file).toString());
-      });
-    });
-  }
-  if (args[0] == "findProviders") {
-    // peer.contentRouting.findProviders(new Cid(args[1]), (err, providers) => {
-    //   if (err) {
-    //     throw err;
-    //   }
-    //   console.log(
-    //     "Found providers:\n",
-    //     providers.map(p => p.id.toB58String()).join("\n")
-    //   );
-    // });
-  }
-  if (args[0] == "ping") {
-    console.log("pong");
-  }
-}
--- models/node.js
+++ models/node.js
@@ -5,10 +5,19 @@ const SPDY = require("libp2p-spdy");
 const Multiplex = require("libp2p-mplex");
 const SECIO = require("libp2p-secio");
 const Bootstrap = require("libp2p-bootstrap");
+const crypto = require("crypto");
+const _ = require("underscore");
+const toPull = require("stream-to-pull-stream");
+const Readable = require("stream").Readable;
+const glob = require("glob");
+const fs = require("fs");
+const pull = require("pull-stream");
 
 const defaultsDeep = require("@nodeutils/defaults-deep");
 
 const bootstrapers = [];
+
+const chunkQuantity = 2;
 
 const DEFAULT_OPTS = {
   modules: {
@@ -40,6 +49,125 @@ class Node extends Libp2p {
     const bootstrap_opts = DEFAULT_OPTS;
     bootstrap_opts.config.peerDiscovery.bootstrap.list = bootstrap_list;
     super(defaultsDeep(opts, bootstrap_opts));
+  }
+
+  addListeners() {
+    this.on("error", err => {
+      console.error("libp2p error: ", err);
+      throw err;
+    });
+
+    this.on("peer:connect", peer => {
+      console.log("Connection established to:", peer.id.toB58String());
+    });
+
+    this.handle("/file/1.0.0", (protocolName, connection) => {
+      pull(
+        connection,
+        pull.collect((err, data) => {
+          console.log("received:\n", data.toString());
+          var num = data[0];
+          var hash = data[1].toString();
+          var shard = data[2].toString();
+          var id = this.peerInfo.id.toB58String();
+          fs.writeFile(`shards/${id}_${hash}_${num}`, shard, function(err) {
+            if (err) throw err;
+            console.log("File is created successfully.");
+            // peer.contentRouting.provide(new Cid(1, "raw", data[1]), err => {
+            //   if (err) throw err;
+
+            //   console.log("Node %s is providing %s", id, hash);
+            // });
+          });
+        })
+      );
+    });
+  }
+
+  async dhtPut(key, value) {
+    const k = Buffer.from(key);
+    const v = Buffer.from(value);
+    try {
+      await this.dht.put(k, v);
+      console.log(`PUT: (${k},${v})`);
+    } catch (e) {
+      console.log(`PUT: (${k},${v}) -> ${e.message}`);
+    }
+  }
+  async dhtGet(key) {
+    const k = Buffer.from(key);
+    try {
+      const v = await this.dht.get(k);
+      console.log(`GET: (${k},${v})`);
+      return v;
+    } catch (e) {
+      console.log(`GET: (${k}) -> ${e.message}`);
+    }
+  }
+  async addMetaData(hash) {
+    var metaData = await this.dhtGet("0");
+    metaData = metaData ? JSON.parse(metaData) : [];
+    metaData.push(hash);
+    this.dhtPut("0", JSON.stringify(metaData));
+  }
+
+  splitString(str, n) {
+    var chunks = [];
+    var quantity = Math.ceil(str.length / n);
+    for (var i = 0, charsLength = str.length; i < charsLength; i += quantity) {
+      chunks.push(str.substring(i, i + quantity));
+    }
+    return chunks;
+  }
+
+  async storeFile(fileName) {
+    const data = fs.readFileSync(fileName, "utf8");
+    console.log(`FILE: ${fileName}\n${data}`);
+
+    const dataHash = crypto
+      .createHash("md5")
+      .update(data)
+      .digest("hex");
+
+    const chunks = this.splitString(data, chunkQuantity);
+    var peers = this.peerBook.getAllArray();
+    if (peers.length < chunkQuantity)
+      throw new Error(
+        `Not enough peers in the network (${peers.length} < ${chunkQuantity})`
+      );
+    _.sample(peers, chunkQuantity).forEach((p, i) => {
+      var s = new Readable();
+      s.push(i.toString());
+      s.push(dataHash);
+      s.push(chunks[i]);
+      s.push(null);
+
+      this.dialProtocol(p, "/file/1.0.0", (err, connection) => {
+        pull(toPull.duplex(s), connection);
+      });
+    });
+    this.addMetaData(dataHash);
+  }
+
+  getPeerChunks() {
+    var id = this.peerInfo.id.toB58String();
+
+    glob(`shards/${id}*`, function(er, files) {
+      files.forEach(file => {
+        console.log(file, "\n", fs.readFileSync(file).toString());
+      });
+    });
+  }
+
+  async processCommand(command) {
+    var args = command.split(" ");
+    var fun = args.shift();
+
+    try {
+      console.log(`RESULT: ${await this[fun](...args)}`);
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 
